07663166edbc1a36db91078e2f84099a683bc65c
Sahaya Cyril <51327503+sahaya-cyril@users.noreply.github.com>
2021-03-03 20:07:07 +0530

    Implement Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,13 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
